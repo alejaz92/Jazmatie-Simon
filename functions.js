f3fd095cae74ff61bfef3a6137e137d88428daac
alejaz92 <alejandro.jaz92@gmail.com>
2023-07-21 16:34:34 -0300

    Funcion sombreado al clickear botones

--- functions.js
+++ functions.js
@@ -12,3 +12,47 @@ function checkNombre() {
         
     }
 }
+
+function clickRojo() {
+    var area =   document.getElementById("area-rojo")
+    
+    area.style.opacity = 0.7;
+    setTimeout(function () {
+        area.style.opacity = 1
+    }, 250);
+
+    
+}
+
+function clickVerde() {
+    var area =   document.getElementById("area-verde")
+    
+    area.style.opacity = 0.7;
+    setTimeout(function () {
+        area.style.opacity = 1
+    }, 250);
+
+    
+}
+
+function clickAzul() {
+    var area =   document.getElementById("area-azul")
+    
+    area.style.opacity = 0.7;
+    setTimeout(function () {
+        area.style.opacity = 1
+    }, 250);
+
+    
+}
+
+function clickAmarillo() {
+    var area =   document.getElementById("area-amarillo")
+    
+    area.style.opacity = 0.7;
+    setTimeout(function () {
+        area.style.opacity = 1
+    }, 250);
+
+    
+}
