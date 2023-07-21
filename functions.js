4c75f60a57a813b051523c40a78fecd0a87e50a7
alejaz92 <alejandro.jaz92@gmail.com>
2023-07-21 16:53:56 -0300

    Avance logica juego

--- functions.js
+++ functions.js
@@ -1,3 +1,9 @@
+var contSecuencia = 1;
+var estadoJuego = false;
+var secuenciaJuego = [];
+var posicionJuego = 0
+
+
 function checkNombre() {
     var nombre = document.getElementById("menu-nombre")
 
@@ -7,52 +13,105 @@ function checkNombre() {
     }
     else {
         //iniciar juego
-        alert('Iniciando Juego');v
+        
        nombre.disabled = true;
+       estadoJuego = true;
+       juego();
         
     }
 }
 
 function clickRojo() {
-    var area =   document.getElementById("area-rojo")
+    if (estadoJuego == true) {
+        var area =   document.getElementById("area-rojo")
     
-    area.style.opacity = 0.7;
-    setTimeout(function () {
-        area.style.opacity = 1
-    }, 250);
-
+        area.style.opacity = 0.7;
+        setTimeout(function () {
+            area.style.opacity = 1
+        }, 250);    
+    }
+   
     
 }
 
 function clickVerde() {
-    var area =   document.getElementById("area-verde")
+    if (estadoJuego == true) {
+        var area =   document.getElementById("area-verde")
     
-    area.style.opacity = 0.7;
-    setTimeout(function () {
-        area.style.opacity = 1
-    }, 250);
-
+        area.style.opacity = 0.7;
+        setTimeout(function () {
+            area.style.opacity = 1
+        }, 250);    
+    }
+   
     
 }
 
 function clickAzul() {
-    var area =   document.getElementById("area-azul")
+    if (estadoJuego == true) {
+        var area =   document.getElementById("area-azul")
     
-    area.style.opacity = 0.7;
-    setTimeout(function () {
-        area.style.opacity = 1
-    }, 250);
-
+        area.style.opacity = 0.7;
+        setTimeout(function () {
+            area.style.opacity = 1
+        }, 250);    
+    }
+   
     
 }
 
-function clickAmarillo() {
-    var area =   document.getElementById("area-amarillo")
-    
-    area.style.opacity = 0.7;
-    setTimeout(function () {
-        area.style.opacity = 1
-    }, 250);
 
+function clickAmarillo() {
+    if (estadoJuego == true) {
+        var area =   document.getElementById("area-amarillo")
+    
+        area.style.opacity = 0.7;
+        setTimeout(function () {
+            area.style.opacity = 1
+        }, 250);    
+    }
+   
+    
+}
+
+function juego() {    
+
+    
+    for (var i = 1; i <= contSecuencia; i++) {
+        var valorRand = Math.ceil(Math.random() * 4)
+        secuenciaJuego.push(valorRand);
+        if (valorRand == 1) {
+            var area =   document.getElementById("area-verde")
+    
+            area.style.opacity = 0.7;
+            setTimeout(function () {
+                area.style.opacity = 1
+            }, 250);    
+        }
+        else if (valorRand == 2) {
+            var area =   document.getElementById("area-rojo")
+    
+            area.style.opacity = 0.7;
+            setTimeout(function () {
+                area.style.opacity = 1
+            }, 250);
+        }
+        else if (valorRand == 3) {
+            var area =   document.getElementById("area-amarillo")
+    
+            area.style.opacity = 0.7;
+            setTimeout(function () {
+                area.style.opacity = 1
+            }, 250);
+        }
+        else if (valorRand == 4) {
+            var area =   document.getElementById("area-azul")
+    
+            area.style.opacity = 0.7;
+            setTimeout(function () {
+                area.style.opacity = 1
+            }, 250);
+        }
+    }
     
 }
